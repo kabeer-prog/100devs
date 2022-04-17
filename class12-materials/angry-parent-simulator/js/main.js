92fbded664e637c66383ed9fc5c5bf8ccc1835d6
kabeer-prog <kabeerlap@gmail.com>
2022-04-17 14:00:00 +0100

    100devs file

--- class12-materials/angry-parent-simulator/js/main.js
+++ class12-materials/angry-parent-simulator/js/main.js
@@ -1,9 +1,9 @@
-document.querySelector('#yell').addEventListener('click', run)
+document.querySelector('#yell').addEventListener('click', run);
 
 function run() {
-  const fName = document.querySelector('#firstName').value
-  const fMidName = document.querySelector('#firstMiddle').value
-  const lMidName = document.querySelector('#lastMiddle').value
+  const fName = document.querySelector('#firstName').value;
+  const fMidName = document.querySelector('#firstMiddle').value;
+  const lMidName = document.querySelector('#lastMiddle').value;
   const lName = document.querySelector('#lastName').value
 
   document.querySelector('#placeToYell').innerText = fName + ' ' + fMidName + ' ' + ' ' + lMidName + ' ' + lName
