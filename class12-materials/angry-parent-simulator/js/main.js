887beaa2a731eaa31da0c0613dca630004e03a18
kabeer-prog <kabeerlap@gmail.com>
2022-04-18 14:00:00 +0100

    100devs file

--- class12-materials/angry-parent-simulator/js/main.js
+++ class12-materials/angry-parent-simulator/js/main.js
@@ -4,9 +4,9 @@ function run() {
   const fName = document.querySelector('#firstName').value;
   const fMidName = document.querySelector('#firstMiddle').value;
   const lMidName = document.querySelector('#lastMiddle').value;
-  const lName = document.querySelector('#lastName').value
+  const lName = document.querySelector('#lastName').value;
 
-  document.querySelector('#placeToYell').innerText = fName + ' ' + fMidName + ' ' + ' ' + lMidName + ' ' + lName
+  document.querySelector('#placeToYell').innerText = fName + ' ' + fMidName + ' ' + ' ' + lMidName + ' ' + lName;
 
   //Add what you should be doing - conditionals go here
 
