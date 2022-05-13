35409f370e09780e83987cf36383425e24c58f65
kabeer-prog <kabeerlap@gmail.com>
2022-05-13 14:00:00 +0100

    100devs files

--- class12-materials/simple-calculator/js/main.js
+++ class12-materials/simple-calculator/js/main.js
@@ -1,6 +1,6 @@
 let total = 0
 
-document.querySelector('#pumpkin').addEventListener('click', makeZero)
+document.querySelector('#pumpkin').addEventListener('click', makeZero);
 document.querySelector('#dominosPizza').addEventListener('click', jumanji)
 document.querySelector('#zebra').addEventListener('click', add9)
 document.querySelector('#cantThinkOfAnything').addEventListener('click', sub2)
@@ -17,10 +17,10 @@ function jumanji() {
 
 function add9() {
   total = total + 9
-  document.querySelector('#placeToPutResult').innerHTML = total
+  document.querySelector('#placeToPutResult').innerHTML = total;
 }
 
 function sub2() {
   total = total - 2
-  document.querySelector('#placeToPutResult').innerHTML = total
+  document.querySelector('#placeToPutResult').innerHTML = total;
 }
