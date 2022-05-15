966b47b84c49fc1220fc15bde19f6efea4d3c372
kabeer-prog <kabeerlap@gmail.com>
2022-05-15 14:00:00 +0100

    100dev files

--- class12-materials/background-picker/js/main.js
+++ class12-materials/background-picker/js/main.js
@@ -1,6 +1,6 @@
-document.getElementById('purple').onclick = partyPurple
-document.getElementById('green').onclick = partyGreen
-document.getElementById('blue').onclick = partyBlue
+document.getElementById('purple').onclick = partyPurple;
+document.getElementById('green').onclick = partyGreen;
+document.getElementById('blue').onclick = partyBlue;
 
 
 function partyPurple() {
